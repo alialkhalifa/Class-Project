42d014e66223e0dd7d1ba645d90a3243b9ce6525
alialkhalifa <152819260+alialkhalifaa@users.noreply.github.com>
2024-12-04 14:15:49 -0500

    Updated solution

--- solution.py
+++ solution.py
@@ -1,1 +1,15 @@
 userInput = input("Enter a password: ")
+dynamic_input = userInput
+temp = ""
+passwords = []
+char_count = 0
+
+with open("words.txt", "r") as f:
+    for line in f.readlines():
+        for char in dynamic_input:
+             temp += char
+             char_count += 1
+             if temp == line:
+                 passwords.append(temp)
+                 dynamic_input = userInput[char_count:]
+            
